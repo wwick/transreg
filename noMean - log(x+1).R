18a9ab076277fd1cf26cf763931b4ec87c7113f8
wwick <williamrobertwick@gmail.com>
2017-07-18 14:15:36 -0700

    fixed stuff

---
+++ noMean - log(x+1).R
@@ -0,0 +1,81 @@
+start <- Sys.time()
+
+file.path <- "~\\projects\\transcriptomics\\data\\"
+file.name <- "expressionMatrix.csv"
+
+q = 0.970 # 95% confidence
+
+
+
+# Dixon's Q Test
+Outlier <- function(x) {
+  x <- sort(x)
+  q.max <- (x[3] - x[2]) / (x[2] - x[1])
+  q.min <- (x[2] - x[1]) / (x[3] - x[2])
+  if (!is.na(q.max) && q.max > q.min && q.max > q) {
+    x[3] <- NA
+    return(x)
+  }
+  if (!is.na(q.min) && q.min > q) {
+    x[1] <- NA
+    return(x)
+  }
+  return(x)
+}
+
+Graph <- function(fit) {
+  title(xlab = 'mRNA (TPM)', ylab = 'RPF (TPM)')
+  abline(fit, col = 'blue')
+
+  r2 <- round(summary(fit)$r.squared, 2)
+  text(3e-1, 1e5, paste('r2 =', r2))
+
+  m <- round(coef(fit)[2], 3)
+  b <- round(coef(fit)[1], 3)
+  text(3e-1, 1e4, paste('y = ', m, 'x + ', b, sep = ''))
+
+  f <- summary(fit)$fstatistic
+  p <- signif(pf(f[1], f[2], f[3], lower.tail = FALSE), 3)
+  text(3e-1, 1e3, paste('p =', p))
+}
+
+file <- paste(file.path, file.name, sep = '')
+pre.data <- read.csv(file = file, row.names = 1)
+
+for (i in 1:8) {
+  apply(cbind(pre.data[,i * 3 - 2], pre.data[,i * 3 - 1], pre.data[,i * 3]),
+    1, Outlier)
+}
+
+total.data <- pre.data
+#for (i in 1:24) {
+#  total.data <- total.data[total.data[,i] != 0,]
+#}
+ribo.data <- total.data[!grepl('VNG', rownames(total.data)),]
+
+par(mfrow = c(2, 4))
+xlim <- c(1e-4, 1e6)
+ylim <- xlim
+
+for (i in 1:4) {
+  x <- (c(total.data[,i * 3 - 2], total.data[,i * 3 - 1],
+     total.data[,i * 3]) + 1)
+  y <- (c(total.data[,i * 3 - 2 + 12], total.data[,i * 3 - 1 + 12],
+    total.data[,i * 3 + 12]) + 1)
+  fit <- lm(log10(y) ~ log10(x))
+  plot(x, y, log = 'xy', xlab = '', ylab = '', xlim = xlim, ylim = ylim,
+    main = paste('Total Time Point', i))
+  Graph(fit)
+}
+
+for (i in 1:4) {
+  x <- c(ribo.data[,i * 3 - 2], ribo.data[,i * 3 - 1], ribo.data[,i * 3]) + 1
+  y <- c(ribo.data[,i * 3 - 2 + 12], ribo.data[,i * 3 - 1 + 12], ribo.data[,i * 3 + 12]) + 1
+  fit <- lm(log10(y) ~ log10(x))
+  plot(x, y, log = 'xy', xlab = '', ylab = '', xlim = xlim, ylim = ylim,
+    main = paste('Ribosomal Time Point', i))
+  Graph(fit)
+}
+
+end <- Sys.time()
+print(end - start)
---
+++ residDensOne.R
@@ -0,0 +1,91 @@
+start <- Sys.time()
+
+file.path <- "~\\projects\\transcriptomics\\data\\"
+file.name <- "expressionMatrix.csv"
+
+q = 0.970 # 95% confidence
+
+Col <- function(x) {
+  apply(cbind(pre.data[,x * 3 - 2], pre.data[,x * 3 - 1], pre.data[,x * 3]),
+    1, Outlier)
+}
+
+Minus <- function(x) {
+  return(log10(x[2] / x[1]))
+}
+
+# Dixon's Q Test
+Outlier <- function(x) {
+  x <- sort(x)
+  q.max <- (x[3] - x[2]) / (x[2] - x[1])
+  q.min <- (x[2] - x[1]) / (x[3] - x[2])
+  if (!is.na(q.max) && q.max > q.min && q.max > q) {
+    x[3] <- NA
+    return(mean(x, na.rm = TRUE))
+  }
+  if (!is.na(q.min) && q.min > q) {
+    x[1] <- NA
+    return(mean(x, na.rm = TRUE))
+  }
+  return(mean(x))
+}
+
+Graph <- function(fit) {
+  title(xlab = 'mRNA (TPM)', ylab = 'RPF (TPM)')
+  abline(fit, col = 'blue')
+
+  r2 <- round(summary(fit)$r.squared, 2)
+  text(3e-1, 1e5, paste('r2 =', r2))
+
+  m <- round(coef(fit)[2], 3)
+  b <- round(coef(fit)[1], 3)
+  text(3e-1, 1e4, paste('y = ', m, 'x + ', b, sep = ''))
+
+  f <- summary(fit)$fstatistic
+  p <- signif(pf(f[1], f[2], f[3], lower.tail = FALSE), 3)
+  text(3e-1, 1e3, paste('p =', p))
+}
+
+file <- paste(file.path, file.name, sep = '')
+pre.data <- read.csv(file = file, row.names = 1)
+
+total.data <- list()
+ribo.data <- list()
+for (i in 1:4) {
+  total.data[[i]] <- cbind(Col(i * 2 - 1), Col(i * 2))
+  rownames(total.data[[i]]) <- rownames(pre.data)
+  total.data[[i]] <- total.data[[i]][total.data[[i]][,1] != 0,]
+  total.data[[i]] <- total.data[[i]][total.data[[i]][,2] != 0,]
+  ribo.data[[i]] <- total.data[[i]][!grepl('VNG', rownames(total.data[[i]])),]
+}
+
+par(mfrow = c(2, 4))
+xlim <- c(1e-4, 1e6)
+ylim <- xlim
+
+for (i in 1:4) {
+  x <- total.data[[i]][,1]
+  y <- total.data[[i]][,2]
+  fit <- lm(log10(y) ~ log10(x))
+  resid <- apply(total.data[[i]], 1, Minus)
+  plot(density(resid), main = paste('Total Time Point', i))
+#  plot(x, y, log = 'xy', xlab = '', ylab = '', xlim = xlim, ylim = ylim,
+#    main = paste('Total Time Point', i))
+#  Graph(fit)
+}
+
+for (i in 1:4) {
+  x <- ribo.data[[i]][,1]
+  y <- ribo.data[[i]][,2]
+  fit <- lm(log10(y) ~ log10(x))
+  resid <- apply(ribo.data[[i]], 1, Minus)
+  plot(density(resid), main = paste('Ribosomal Time Point', i))
+#  plot(x, y, log = 'xy', xlab = '', ylab = '', xlim = xlim, ylim = ylim,
+#    main = paste('Ribosomal Time Point', i))
+#  Graph(fit)
+}
+
+
+
+end <- Sys.time()
+print(end - start)
